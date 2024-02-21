f13c6153ad2d818d849abd889cf75a86892ee48b
Mohsin Raees <160493218+MohsinRaees@users.noreply.github.com>
2024-02-22 02:59:20 +0500

    Add files via upload

---
+++ NoUsers.js
@@ -0,0 +1,32 @@
+//No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+//• If the list is empty, print the message We need to find some users!
+//• Remove all of the usernames from your array, and make sure the correct message is printed.
+var age = 25;
+if (age < 2) {
+    console.log("The person is a baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("The person is a toddler.");
+}
+else if (age >= 4 && age < 13) {
+    console.log("The person is a kid.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("The person is a teenager.");
+}
+else if (age >= 20 && age < 65) {
+    console.log("The person is an adult.");
+}
+else {
+    console.log("The person is an elder.");
+}
+// No Users
+var userList = ["User1", "User2", "User3"];
+if (userList.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    // Remove all usernames from the array
+    userList = [];
+    console.log("Usernames removed. The list is now empty.");
+}
